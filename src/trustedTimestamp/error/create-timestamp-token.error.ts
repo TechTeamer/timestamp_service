45766af875b75679679dabf124d78d37ce115880
95gabor <gabor.pichner@facekom.com>
2025-09-15 13:25:03 +0200

    chore(eslint): eslint prettier plugin

--- src/trustedTimestamp/error/create-timestamp-token.error.ts
+++ src/trustedTimestamp/error/create-timestamp-token.error.ts
@@ -1,13 +1,13 @@
-import { CreatedTimestampToken } from '../types/timestamp-token.type.js';
+import { CreatedTimestampToken } from '../types/timestamp-token.type.js'
 
-export type CreateTimestampTokenErrorContext = Partial<Pick<CreatedTimestampToken, 'logHistory' | 'providerName'>>;
+export type CreateTimestampTokenErrorContext = Partial<Pick<CreatedTimestampToken, 'logHistory' | 'providerName'>>
 
 export class CreateTimestampTokenError extends Error {
   constructor(
     message: string,
     readonly context: CreateTimestampTokenErrorContext = {},
-    options?: ErrorOptions,
+    options?: ErrorOptions
   ) {
-    super(message, options);
+    super(message, options)
   }
 }
